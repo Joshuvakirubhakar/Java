afb17d5bf713e15da1ea60dc6cb0dc77761a6fcb
Joshuvakirubhakar <joshuavictor99@gmail.com>
2022-05-12 13:08:33 +0530

    String rotation

---
+++ StringRotation/src/DoubleRotation.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class DoubleRotation {
+
+	
+
+	public static String LR(String s, int L) {
+		int a = s.length();
+		String s1 = "";
+		if (L > a) {
+			int b = L % a;
+			s1 = s.substring(b) + s.substring(0, b);
+			return s1;
+		} else {
+			s1 = s.substring(L) + s.substring(0, L);
+			return s1;
+		}
+
+	}
+
+	public static String RR(String s, int R) {
+		int a = s.length(), c;
+		String s1 = "";
+		if (R > a) {
+			int b = R % a;
+			c = a - b;
+			s1 = s.substring(c) + s.substring(0, c);
+			return s1;
+		} else {
+			int b = a - R;
+			s1 = s.substring(b) + s.substring(0, b);
+			return s1;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String g = "abcde";
+		int L = sc.nextInt();
+		int R = sc.nextInt();
+		String g1 = Rotation.LR(g, L); 
+		System.out.println(Rotation.RR(g1, R));
+		sc.close();
+
+	}
+
+
+
+}
+
+
